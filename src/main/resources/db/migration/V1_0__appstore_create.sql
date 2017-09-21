25d8ed3a03c6ee8358983781e84839049a486f32
Lucas Holt <luke@foolishgames.com>
2017-09-21 18:42:19 -0400

    add missing mapping tables

--- src/main/resources/db/migration/V1_0__appstore_create.sql
+++ src/main/resources/db/migration/V1_0__appstore_create.sql
@@ -2,14 +2,14 @@ CREATE TABLE architecture (
   id          SERIAL PRIMARY KEY,
   name        VARCHAR(30) NOT NULL,
   description TEXT,
-  created    TIMESTAMP
+  created     TIMESTAMP
 );
 
 CREATE TABLE category (
   id          SERIAL PRIMARY KEY,
   name        VARCHAR(30) NOT NULL,
   description TEXT,
-  created    TIMESTAMP
+  created     TIMESTAMP
 );
 
 CREATE TABLE license (
@@ -17,14 +17,14 @@ CREATE TABLE license (
   name        VARCHAR(100) NOT NULL,
   description TEXT,
   url         VARCHAR(255),
-  created    TIMESTAMP
+  created     TIMESTAMP
 );
 
 CREATE TABLE os (
   id      SERIAL PRIMARY KEY,
   name    VARCHAR(30) NOT NULL DEFAULT 'MidnightBSD',
   version VARCHAR(10) NOT NULL,
-  created    TIMESTAMP
+  created TIMESTAMP
 );
 
 CREATE TABLE package (
@@ -32,23 +32,33 @@ CREATE TABLE package (
   name        VARCHAR(255) NOT NULL,
   description TEXT,
   url         VARCHAR(255),
-  created    TIMESTAMP
+  created     TIMESTAMP
 );
 
 CREATE TABLE package_instance (
   id              SERIAL PRIMARY KEY,
   version         VARCHAR(255) NOT NULL,
-  os_id           INT,
-  architecture_id INT,
-  package_id      INT,
-  created    TIMESTAMP
+  os_id           INT REFERENCES os (id),
+  architecture_id INT REFERENCES architecture (id),
+  package_id      INT REFERENCES package (id),
+  created         TIMESTAMP
 );
 
 CREATE TABLE rating (
   id         SERIAL PRIMARY KEY,
-  package_id INT,
+  package_id INT REFERENCES package (id),
   author     VARCHAR(255),
   comment    VARCHAR(500),
   score      INT,
   created    TIMESTAMP
+);
+
+CREATE TABLE package_category_map (
+  package_id  INT REFERENCES package (id),
+  category_id INT REFERENCES category (id)
+);
+
+CREATE TABLE package_instance_license_map (
+  packge_instance_id INT REFERENCES package_instance (id),
+  license_id         INT REFERENCES license (id)
 );
