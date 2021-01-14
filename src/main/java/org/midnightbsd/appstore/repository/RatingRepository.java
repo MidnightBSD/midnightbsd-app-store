4ec657c961264bf56a0a67f2d9db623b73e995d3
Lucas Holt <luke@foolishgames.com>
2021-01-14 12:40:43 -0500

    cleanup

--- src/main/java/org/midnightbsd/appstore/repository/RatingRepository.java
+++ src/main/java/org/midnightbsd/appstore/repository/RatingRepository.java
@@ -1,20 +1,17 @@
 package org.midnightbsd.appstore.repository;
 
 import org.midnightbsd.appstore.model.Rating;
-import org.midnightbsd.appstore.model.RatingAverage;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.math.BigDecimal;
 import java.util.List;
 
 /**
  * @author Lucas Holt
  */
 @Repository
-public interface RatingRepository extends JpaRepository<Rating, Integer>{
+public interface RatingRepository extends JpaRepository<Rating, Integer> {
 
     List<Rating> findAllByPkg(@Param("pkg") Package pkg);
 
