75c2b986f7adc4618fc2be3a2dbcc79efc727eb2
Lucas Holt <luke@foolishgames.com>
2022-03-31 16:02:24 -0400

    spring4shell mitigation

---
+++ src/main/java/org/midnightbsd/appstore/ctl/BinderControllerAdvise.java
@@ -0,0 +1,22 @@
+package org.midnightbsd.appstore.ctl;
+
+import org.springframework.core.annotation.Order;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.InitBinder;
+
+/**
+ * Mitigation for spring4shell
+ */
+@ControllerAdvice
+@Order(10000)
+public class BinderControllerAdvise {
+    @InitBinder
+    public void setAllowedFields(WebDataBinder dataBinder) {
+        String [] denylist = new String[] {
+                "class.*", "Class.*", "*.class.*", "*.Class.*"
+        };
+
+        dataBinder.setDisallowedFields(denylist);
+    }
+}
