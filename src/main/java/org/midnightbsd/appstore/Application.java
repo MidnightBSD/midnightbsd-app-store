2c263804fedcca864ad411e6f24001d3035f727e
Lucas Holt <luke@foolishgames.com>
2025-03-02 03:12:13 -0500

    drop tomcat back down. latest doesn't work.
    remove spring cloud dependency.

--- src/main/java/org/midnightbsd/appstore/Application.java
+++ src/main/java/org/midnightbsd/appstore/Application.java
@@ -2,7 +2,6 @@ package org.midnightbsd.appstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -13,7 +12,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
  * MidnightBSD App Store
  * @author Lucas Holt
  */
-@EnableEurekaClient
 @Configuration
 @EnableScheduling
 @EnableAsync
